5bcf9478852c4ee5478af2e56abff1d04e7f2696
João Reis <johndivision@live.com.pt>
2016-12-22 22:19:16 +0000

    DAOs

--- Fase 2/DivideDespesa/src/dividedespesa/database/DespesaDAO.java
+++ Fase 2/DivideDespesa/src/dividedespesa/database/DespesaDAO.java
@@ -6,9 +6,13 @@
 package dividedespesa.database;
 
 import dividedespesa.Despesa;
+import dividedespesa.Morador;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Collection;
+import java.util.GregorianCalendar;
 import java.util.Map;
 import java.util.Set;
 
@@ -16,73 +20,49 @@ import java.util.Set;
  *
  * @author João
  */
-public class DespesaDAO implements Map<Integer, Despesa> {
-
-    public void toDB(Despesa d) throws SQLException, ClassNotFoundException{
+public class DespesaDAO {
+    
+    public void put(Despesa despesa, Morador m) throws SQLException{
         Connection c = Connect.connect();
-        PreparedStatment st = c.prepareStatment("INSERT INTO despesa VALUES(?,?,?,?,?)")
+        PreparedStatement st = c.prepareStatement("INSERT INTO despesa VALUES(?,?,?,?,?,?,?)");
+        
+        java.sql.Date dataEmissao = new java.sql.Date(despesa.getDataEmissao().getTimeInMillis());
+        java.sql.Date dataLimite = new java.sql.Date(despesa.getDataLimite().getTimeInMillis());
+        java.sql.Date dataPagamento = new java.sql.Date(despesa.getDataPagamento().getTimeInMillis());
+        
+        st.setInt(1,despesa.getNumDespesa());
+        st.setDouble(2,despesa.getValor());
+        st.setDate(3,dataEmissao);
+        st.setDate(4,dataLimite);
+        st.setDate(5,dataPagamento);        
+        st.setString(5,despesa.getTipoDespesaString()); // possivelmente mal
+        st.setString(6, m.getUsername());
+        st.executeUpdate();
+        
+        c.close();
     }
     
-    @Override
-    public int size() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        public Despesa get(Object key) {
+        Despesa d = null;
+        Connection con = null;
+        try {
+            con = Connect.connect();
+            
+            // OBTEM DADOS DO QUARTO
+            PreparedStatement ps = con.prepareStatement("select * from despesa where id = ?");
+            ps.setInt(1, Integer.parseInt(key.toString()));
+            ResultSet rs = ps.executeQuery();
+            GregorianCalendar dataEmissao = new GregorianCalendar(), dataLimite = new GregorianCalendar(), dataPagamento = new GregorianCalendar();
+   
+            if(rs.next())
+                dataEmissao.setTime(rs.getDate("data_emissao"));
+                dataLimite.setTime(rs.getDate("data_limite"));
+                dataPagamento.setTime(rs.getDate("data_pagamento"));
+                c = new Despesa(rs.getInt("id"),rs.getDouble("numero_moradores"), dataEmissao, dataLimite, dataPagamento, rs.getString("tipo"));
+            
+            con.close();
+        } catch (SQLException e) {}
+        return c;
     }
-
-    @Override
-    public boolean isEmpty() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public boolean containsKey(Object key) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public boolean containsValue(Object value) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Despesa get(Object key) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Despesa put(Integer key, Despesa value) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Despesa remove(Object key) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void putAll(Map<? extends Integer, ? extends Despesa> m) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void clear() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Set<Integer> keySet() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Collection<Despesa> values() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Set<Entry<Integer, Despesa>> entrySet() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
-    
     
 }
--- Fase 2/DivideDespesa/src/dividedespesa/database/MoradorDAO.java
+++ Fase 2/DivideDespesa/src/dividedespesa/database/MoradorDAO.java
@@ -5,10 +5,66 @@
  */
 package dividedespesa.database;
 
+import dividedespesa.Morador;
+import dividedespesa.Quarto;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author João
  */
 public class MoradorDAO {
+    
+    public void put(Morador morador, Quarto quarto) throws SQLException{
+        Connection c = Connect.connect();
+        PreparedStatement st = c.prepareStatement("INSERT INTO morador VALUES(?,?,?,?,?)");
+        
+        java.sql.Date dataEntrada = new java.sql.Date(morador.getDataEntrada().getTimeInMillis());
+        java.sql.Date dataSaida = new java.sql.Date(morador.getDataSaida().getTimeInMillis());
+        
+        st.setString(1,morador.getUsername());
+        st.setString(2,morador.getPassword());
+        st.setString(3,morador.getNome());
+        st.setDate(4,dataEntrada);
+        st.setDate(5,dataSaida);        
+        st.executeUpdate();
+        
+        st = c.prepareStatement("INSERT INTO moradorquarto VALUES(?,?)");
+        st.setInt(1,quarto.getNumQuarto());
+        st.setString(2, morador.getUsername());
+        st.executeUpdate();
+        
+        c.close();
+    }
+    
+    public int size() {
+        int size = -1;
+        try{
+            Connection con = Connect.connect();
+            PreparedStatement ps = con.prepareStatement("select count(id) from morador");
+            ResultSet rs = ps.executeQuery();
+
+            if(rs.next())
+                size = rs.getInt(1);
+
+            con.close();
+        } catch (Exception e){};
+
+        return size;
+    }
+    
+    public void get(String username) throws SQLException{
+        Connection c = Connect.connect();
+        PreparedStatement st_morador = c.prepareStatement("INSERT INTO morador VALUES(?,?,?,?,?)");
+        PreparedStatment st_despesas = c.prepareStatment("SELECT")
+                +
+        
+        Morador m 
+        
+        c.close();
+    }
     
 }
