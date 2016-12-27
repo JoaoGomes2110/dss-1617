f1995213dee2b037b2e9dd3ce8c3fb497e4e6b32
Carlos Pereira <carloscdap@gmail.com>
2016-12-27 17:31:17 +0000

    Adicionada Facade

--- Fase 2/DivideDespesa/src/dividedespesa/DivideDespesaFacade.java
+++ Fase 2/DivideDespesa/src/dividedespesa/DivideDespesaFacade.java
@@ -10,9 +10,40 @@ package dividedespesa;
  * @author Carlos Pereira
  */
 public class DivideDespesaFacade {
-    Apartamento apartamento;
+    DivideDespesa dd;
+    
+    public void DivideDespesaFacade() {
+        dd = new DivideDespesa();
+    }
     
     
+    public boolean validaDadosLogin(String username, String password) {
+        
+        Utilizador aux = new Utilizador(username, password);
+        boolean ret = false;
+        
+        try {
+            aux.validaUsername();
+            aux.validaPassword();
+            
+            ret = true;
+        } catch (CarateresEspeciaisException e) {
+            //ret = false;
+        }
+        
+        return ret; 
+    }
+    
+    public boolean fieldSize(String usernameSenhorio, String usernameAdmin, 
+                             String nomeSenhorio, String nomeAdmin,
+                             String pswrdSenhorio, String pswrdAdmin,
+                             String descApartamento) {
+        
+        return (usernameAdmin.length() == 0 || usernameSenhorio.length() == 0 ||
+                nomeSenhorio.length() == 0 || nomeAdmin.length() == 0 ||
+                pswrdSenhorio.length() == 0 || pswrdAdmin.length() == 0 ||
+                descApartamento.length() == 0);
+    }
     
     
     
