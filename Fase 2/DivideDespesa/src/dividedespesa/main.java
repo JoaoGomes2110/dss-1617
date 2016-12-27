a62e2ed368b732e49c990343d6d3e4ff1d6f6722
Carlos Pereira <carloscdap@gmail.com>
2016-12-27 23:28:18 +0000

    Adicionada a main

---
+++ Fase 2/DivideDespesa/src/dividedespesa/main.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dividedespesa;
+
+import Interface.JanelaInicial;
+import javax.swing.SwingUtilities;
+
+
+/**
+ *
+ * @author Carlos Pereira
+ */
+public class main {
+    
+    static DivideDespesaFacade facade;
+    
+    public static void main(String[] args) {
+        facade = new DivideDespesaFacade();
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                new JanelaInicial(facade).setVisible(true);
+            }
+        });
+    }
+}
